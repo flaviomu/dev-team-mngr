4e4901ff6f12fa842153d48bb100548ee71e7213
flaviomu <flaviomurgiasrvcs@gmail.com>
2018-08-06 14:56:32 +0200

    Imp: add swagger api description

---
+++ src/main/java/com/flaviomu/devteammngr/swagger/SwaggerConfig.java
@@ -0,0 +1,51 @@
+package com.flaviomu.devteammngr.swagger;
+
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.flaviomu.devteammngr.web"))
+                .paths(paths())
+                .paths(PathSelectors.any())
+                .build()
+                .useDefaultResponseMessages(false)
+                /*.globalResponseMessage()*/;
+
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("DEV TEAM MANAGER API")
+                .version("1.0.0")
+                .description("Below is a detailed description of the REST API of the DEV TEAM MANAGER application")
+                .contact(new Contact("FM", "", ""))
+                .license("GPL v3")
+                .licenseUrl("https://www.gnu.org/licenses/gpl-3.0.html")
+                .build();
+    }
+
+    private Predicate<String> paths() {
+        return Predicates.or(regex("/users.*"));
+    }
+
+}
--- src/main/java/com/flaviomu/devteammngr/web/UserController.java
+++ src/main/java/com/flaviomu/devteammngr/web/UserController.java
@@ -3,6 +3,10 @@ package com.flaviomu.devteammngr.web;
 import com.flaviomu.devteammngr.data.entity.User;
 import com.flaviomu.devteammngr.service.external.github.GitHubConnectionService;
 import com.flaviomu.devteammngr.service.domain.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +15,8 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+
+@Api(value = "devteammngr", description = "Operations available on the Dev Team Manager application")
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
@@ -19,46 +25,65 @@ public class UserController {
 
     @Autowired
     private UserService userService;
-
     @Autowired
     private GitHubConnectionService gitHubConnectionService;
 
 
+    @ApiOperation(value = "Add a user", response = User.class)
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     public User createUser(@RequestBody User user) {
         return userService.createUser(user);
     }
 
+    @ApiOperation(value = "Retrieve a user given its 'id' provided in the url", response = User.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 404, message = "User not found.")
+    })
     @RequestMapping(value = "/{userId}", method = RequestMethod.GET)
     @ResponseBody
     public User getUser(@PathVariable(value = "userId") Long userId) {
         return userService.getUser(userId);
     }
 
+    @ApiOperation(value = "Retrieve all the user existing", response = List.class)
     @RequestMapping(method = RequestMethod.GET)
     public List<User> getUsers() {
         return userService.getUsers();
     }
 
+    @ApiOperation(value = "Update an existing user given its 'id' provided in the url", response = User.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 404, message = "User not found"),
+            @ApiResponse(code = 400, message = "Request and/or BodyRequest not correctly formatted")
+    })
     @RequestMapping(value = "/{userId}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     public User updateUserWithPut(@PathVariable(value = "userId") Long userId, @RequestBody User user) {
         return userService.updateUserWithPut(userId, user);
     }
 
+    @ApiOperation(value = "Update an existing user given its 'id' provided in the url", response = User.class)
+    @ApiResponse(code = 404, message = "User not found")
     @RequestMapping(value = "/{userId}", method = RequestMethod.PATCH, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public User updateUserWithPatch(@PathVariable(value = "userId") Long userId, @RequestBody String userJson) {
         return userService.updateUserWithPatch(userId, userJson);
     }
 
+    @ApiOperation(value = "Remove a user given its 'id' provided in the url")
+    @ApiResponse(code = 404, message = "User not found")
     @RequestMapping(value = "/{userId}", method = RequestMethod.DELETE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteUser(@PathVariable(value = "userId") Long userId) {
         userService.deleteUser(userId);
     }
 
+    @ApiOperation(value = "Retrieve an overview of the GitHub repositories of a user given its 'id' provided in the url", response = List.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 404, message = "User not found"),
+            @ApiResponse(code = 500, message = "An unexpected error occurred")
+    })
     @RequestMapping(value = "/{userId}/repositories", method = RequestMethod.GET)
     @ResponseBody
     public List<GHRepositoryOverview> getRepositoriesOverview(@PathVariable(value = "userId") Long userId) {
